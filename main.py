34b2492332e229affd2a183f5a65090730eb0fdf
amplex <43629623+1amplex@users.noreply.github.com>
2023-07-07 14:33:20 +1000

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,18 @@ app = Flask(__name__)
 @app.route('/image_data', methods=['GET'])
 def get_image_data():
     img = Image.open('upload.png')
-    img = img.resize((128, 128))  # Ensure the image is 32x32
+
+    # Calculate aspect ratio
+    aspect_ratio = img.width / img.height
+
+    if img.width > img.height:
+        new_width = 128
+        new_height = int(new_width / aspect_ratio)
+    else:
+        new_height = 128
+        new_width = int(new_height * aspect_ratio)
+
+    img = img.resize((new_width, new_height))
 
     image_data = np.array(img).tolist()
 
@@ -21,7 +32,18 @@ def request_img_upload():
 
     if file_path:
         img = Image.open(file_path)
-        img.resize((128, 128))
+
+        # Calculate aspect ratio
+        aspect_ratio = img.width / img.height
+
+        if img.width > img.height:
+            new_width = 128
+            new_height = int(new_width / aspect_ratio)
+        else:
+            new_height = 128
+            new_width = int(new_height * aspect_ratio)
+
+        img = img.resize((new_width, new_height))
 
         if os.path.exists('upload.png'):
             os.remove('upload.png')
